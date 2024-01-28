6de4c4f9f82237785345dd2f8fa1ed88cb8c03e4
murugan123456 <131577855+murugan123456@users.noreply.github.com>
2024-01-28 20:34:43 +0530

    Add files via upload

---
+++ Assignment-1/Assignment1.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Assignment1 {
+    public static void main(String[] args) {
+      // byte total 
+      byte b1=20,b2=10,b3=(byte)170,b4=(byte)140;
+      byte total =(byte)(b1+b2+b3+b4);
+      System.out.println("Byte Total value = "+total);
+      //short total 
+      short s1=1300,s2=1200,s3=(short)32900,s4=(short)33000;
+      short total2 = (short)(s1+s2+s3+s4);
+      System.out.println("Short total value = "+total2);
+      //float total 
+      float f1=32.45f,f2=40.23f,f3=70.0320f;
+      float total3=(f1+f2+f3);
+      System.out.println("float total value = "+total3);
+      //double total 
+      double d1=5.0980,d2=9.2309;
+      double total4 =d1+d2;
+      System.out.println("double total value = "+total4);
+  }
+}
